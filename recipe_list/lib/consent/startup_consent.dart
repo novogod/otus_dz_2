b8ccc08ce557d872f1f0e9021f01aebacb601125
novogod <novogod@gmail.com>
2026-05-09 00:52:13 -0400

    fix: bump consent version to force modal re-acceptance

--- recipe_list/lib/consent/startup_consent.dart
+++ recipe_list/lib/consent/startup_consent.dart
@@ -4,7 +4,7 @@ import '../data/api/recipe_api_config.dart';
 import '../data/local/recipe_db.dart';
 import '../i18n.dart';
 
-const String kStartupConsentCurrentVersion = '2026-05-08-v1';
+const String kStartupConsentCurrentVersion = '2026-05-09-v2';
 
 const String _kConsentPlatformWeb = 'web';
 const String _kConsentPlatformNative = 'native';
